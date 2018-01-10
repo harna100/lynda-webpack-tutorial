269b06603085913482548ba02f1798db10638bc6
Paul Harnack <harna100@mail.chapman.edu>
2018-01-10 15:28:34 -0800

    4-4 Bundling vendor files
    Shows how we can bundle 3rd party vendor files into its own bundle

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { Component } from 'react'
+import { render }from 'react-dom'
+
+class Display extends Component {
+    render(){
+        return <div>Important Announcement</div>
+    }
+}
+
+
+render(<Display/>, document.getElementById('react-container'));
